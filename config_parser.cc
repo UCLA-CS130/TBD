52e3b66022a1bc2abfa0f13feded2c625f23f2f8
Derek Nguyen <tincan24@gmail.com>
2017-01-10 13:22:31 -0800

    Added unit tests for normal strings, valid block, invalid block, and fixed bug for invalid blocks

--- config_parser.cc
+++ config_parser.cc
@@ -152,6 +152,8 @@ bool NginxConfigParser::Parse(std::istream* config_file, NginxConfig* config) {
   config_stack.push(config);
   TokenType last_token_type = TOKEN_TYPE_START;
   TokenType token_type;
+  int startEndBlockCount = 0;;
+  
   while (true) {
     std::string token;
     token_type = ParseToken(config_file, &token);
@@ -198,15 +200,19 @@ bool NginxConfigParser::Parse(std::istream* config_file, NginxConfig* config) {
       config_stack.top()->statements_.back().get()->child_block_.reset(
           new_config);
       config_stack.push(new_config);
+      
+			startEndBlockCount++;
     } else if (token_type == TOKEN_TYPE_END_BLOCK) {
       if (last_token_type != TOKEN_TYPE_STATEMENT_END && last_token_type != TOKEN_TYPE_END_BLOCK) {
         // Error.
         break;
       }
       config_stack.pop();
+      
+      startEndBlockCount--;
     } else if (token_type == TOKEN_TYPE_EOF) {
-      if (last_token_type != TOKEN_TYPE_STATEMENT_END &&
-          last_token_type != TOKEN_TYPE_END_BLOCK) {
+      if ((last_token_type != TOKEN_TYPE_STATEMENT_END &&
+          last_token_type != TOKEN_TYPE_END_BLOCK) || startEndBlockCount != 0) {
         // Error.
         break;
       }
--- config_parser_test.cc
+++ config_parser_test.cc
@@ -1,6 +1,17 @@
 #include "gtest/gtest.h"
 #include "config_parser.h"
 
+class NginxConfigParserStringTest : public ::testing::Test {
+	protected:
+		bool ParseString(const std::string config_string) {
+			std::stringstream config_stream(config_string);
+			return parser.Parse(&config_stream, &out_config);
+		}
+		
+		NginxConfigParser parser;
+		NginxConfig out_config;
+};
+
 TEST(NginxConfigParserTest, SimpleConfig) {
   NginxConfigParser parser;
   NginxConfig out_config;
@@ -8,6 +19,50 @@ TEST(NginxConfigParserTest, SimpleConfig) {
   bool success = parser.Parse("example_config", &out_config);
 
   EXPECT_TRUE(success);
+}
+
+TEST(NginxConfigStatementTest, ToStringTest) {
+	NginxConfigStatement statement;
+	statement.tokens_.push_back("foo");
+	statement.tokens_.push_back("bar");
+
+	EXPECT_EQ("foo bar;\n", statement.ToString(0));
+}
+
+/* TODO
+TEST(NginxConfigTest, ToStringTest) {
+	NginxConfigStatement statement;
+	statement.tokens_.push_back("foo");
+	statement.tokens_.push_back("bar");
+
+	NginxConfig config;
+	//std::shared_ptr<NginxConfigStatement> sharedPtr;
+	//sharedPtr.reset(&statement);
+	config.statements_.push_back(std::shared_ptr<NginxConfigStatement>(&statement));
+
+	EXPECT_EQ("foo bar;\n", config.ToString());
+} */
+
+TEST_F(NginxConfigParserStringTest, SimpleConfigTest) {
+	EXPECT_TRUE(ParseString("foo bar;"));
+	EXPECT_EQ(1, out_config.statements_.size());
+	EXPECT_EQ("bar", out_config.statements_[0]->tokens_[1]);
+}
+
+TEST_F(NginxConfigParserStringTest, InvalidNormalString) {
+	ASSERT_FALSE(ParseString("foo bar"));
+}
+
+TEST_F(NginxConfigParserStringTest, ValidBlock) {
+	EXPECT_TRUE(ParseString("location { foo; }"));
+}
+
+TEST_F(NginxConfigParserStringTest, InvalidBlock1) {
+	EXPECT_FALSE(ParseString("location { foo;"));
+}
+
+TEST_F(NginxConfigParserStringTest, InvalidBlock2) {
+	EXPECT_FALSE(ParseString("location foo;}"));
 }
 
 TEST(NginxConfigParserTest, TestConfig) {
