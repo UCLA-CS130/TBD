cc25d101b4f461733f403d787cbdb2bb6ac258f0
Derek Nguyen <tincan24@gmail.com>
2017-01-10 11:33:46 -0800

    separated failed test case into two unit tests

--- config_parser_test.cc
+++ config_parser_test.cc
@@ -6,8 +6,15 @@ TEST(NginxConfigParserTest, SimpleConfig) {
   NginxConfig out_config;
 
   bool success = parser.Parse("example_config", &out_config);
-	bool test1 = parser.Parse("configtest", &out_config);
 
   EXPECT_TRUE(success);
+}
+
+TEST(NginxConfigParserTest, TestConfig) {
+  NginxConfigParser parser;
+  NginxConfig out_config;
+
+	bool test1 = parser.Parse("configtest", &out_config);
+
 	EXPECT_TRUE(test1);
 }
