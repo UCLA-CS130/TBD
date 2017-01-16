fd6df0cc7959fcd273e6a79b2967263f3c9a5998
Derek Nguyen <tincan24@gmail.com>
2017-01-16 14:35:44 -0800

    Refactor runServer and add member variables to Server class

--- server.h
+++ server.h
@@ -1,8 +1,15 @@
 #include <iostream>
+#include "config_parser.h"
+#include <boost/asio.hpp>
 
 class Server {
 	public:
 		Server() {};
 
 		bool runServer(const char* filename);	
+
+    private:
+        NginxConfigParser configParser;
+        NginxConfig config;
+        boost::asio::io_service io_service;
 };
