aec65eec26e5870550df5dd075d00ec1bfa5c5aa
Derek Nguyen <tincan24@gmail.com>
2017-01-30 23:31:33 -0800

    Added checks for valid port value

--- server_config.h
+++ server_config.h
@@ -7,8 +7,16 @@ class ServerConfig {
 public:
     ServerConfig(NginxConfig* config) {
         std::string port_string = config->getConfigValue("listen");
-        if (port_string != "")
+        if (port_string != "") {
             port_ = std::stoi(config->getConfigValue("listen"));
+            if (port_ < 1024 || port_ > 65535) {
+                std::cerr << "The port value must be between 1024 and 65535." << std::endl;
+                exit(1);
+            }
+        } else {
+            std::cerr << "The port must be a valid number." << std::endl;
+            exit(1);
+        }
         static_base_path_ = config->getConfigValue("static_base_path");
     }
     int getPort() {
