91b8ef295b1d8c94ba0150fcac1a125c614872a3
alex m waz <alexmwaz@g.ucla.edu>
2017-02-05 22:32:58 -0800

    Added unit tests for static file handler

--- static_file_handler_test.cc
+++ static_file_handler_test.cc
@@ -8,6 +8,14 @@ protected:
         StaticFileHandler handler(file_path);
         return handler.get_mime_type();
     }
+    std::string build_response(std::string file_path) {
+        StaticFileHandler handler(file_path);
+        return handler.build_response();
+    }
+    std::string read_file(bool &cannot_open_file, std::string file_path) {
+        StaticFileHandler handler(file_path);
+        return handler.read_file(cannot_open_file);
+    }
 };
 
 TEST_F(StaticFileHandlerTest, GetMimeTypeTestJPEG) {
@@ -26,4 +34,26 @@ TEST_F(StaticFileHandlerTest, GetMimeTypeTestDefault) {
     EXPECT_EQ("text/plain", get_mime_type("turtle"));
 }
 
-// TODO: Test read_file and build_response
+TEST_F(StaticFileHandlerTest, ReadFileSuccessTest) {
+    bool cannot_open_file = false;
+    EXPECT_EQ("HEY!\n\n", read_file(cannot_open_file, "test.txt"));
+    EXPECT_FALSE(cannot_open_file);
+}
+
+TEST_F(StaticFileHandlerTest, ReadFileErrorTest) {
+    bool cannot_open_file = false;
+    EXPECT_EQ("", read_file(cannot_open_file, "fail.txt"));
+    EXPECT_TRUE(cannot_open_file);
+}
+
+TEST_F(StaticFileHandlerTest, BuildResponseSuccessTest) {
+    const std::string expectation =
+        "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\n\r\nHEY!\n\n";
+    EXPECT_EQ(expectation, build_response("test.txt"));
+}
+
+TEST_F(StaticFileHandlerTest, BuildResponseErrorTest) {
+    const std::string expectation =
+        "HTTP/1.1 404 Not Found\r\nContent-Type: text/plain\r\n\r\n404 File Not Found\n";
+    EXPECT_EQ(expectation, build_response("fail.txt"));
+}
