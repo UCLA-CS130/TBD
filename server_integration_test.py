19a8a6707add13c988a8cf228246ddc7380a27dd
Jason Yoon <jasony0206@gmail.com>
2017-02-07 04:46:57 +0000

    Update integration test to reflect features in assignment 4

--- server_integration_test.py
+++ server_integration_test.py
@@ -7,28 +7,87 @@ if not os.path.exists("server"):
     subprocess.check_call("make server", stdout=subprocess.PIPE, shell=True)
 
 # Spawn a shell process to run the server
-run_server_command = "./server example_config"
+run_server_command = "./server path_config"
 server_proc = subprocess.Popen(run_server_command, stdout=subprocess.PIPE, shell=True, preexec_fn=os.setsid)
+test_failed = False
 
-# Spawn a shell process to curl to server at port 8080
-run_curl_command = "curl -i localhost:8080"
-curl_proc = subprocess.Popen(run_curl_command, stdout=subprocess.PIPE, shell=True)
-response = curl_proc.stdout.read().decode('utf-8')
+# Echo handler test
+# Spawn a shell process to curl to echo handler endpoint
+echo_request_command = "curl -i localhost:8080/foo"
+echo_request_proc = subprocess.Popen(echo_request_command, stdout=subprocess.PIPE, shell=True)
+echo_response = echo_request_proc.stdout.read().decode('utf-8')
 
-expected_response = """HTTP/1.1 200 OK\r
+expected_echo_response = """HTTP/1.1 200 OK\r
 Content-Type: text/plain\r
 \r
-GET / HTTP/1.1\r
+GET /foo HTTP/1.1\r
 User-Agent: curl/7.35.0\r
 Host: localhost:8080\r
 Accept: */*\r\n\r\n"""
 
+if echo_response != expected_echo_response:
+    print("ERROR: Echo handler replied with an incorrect response.")
+    test_failed = True;
+else:
+    print("SUCCESS: Echo handler replied with the correct response.")
+
+# Static File handler test: mapping static2
+# Spawn a shell process to curl to static file handler endpoint with path under static2/
+static2_file_request_command = "curl -i localhost:8080/static2/test.txt"
+static2_file_request_proc = subprocess.Popen(static2_file_request_command, stdout=subprocess.PIPE, shell=True)
+static2_response = static2_file_request_proc.stdout.read().decode('utf-8')
+
+expected_static2_response = """HTTP/1.1 200 OK\r
+Content-Type: text/plain\r
+\r
+Test file this is\n"""
+
+if static2_response != expected_static2_response:
+    print("ERROR: Static handler replied with an incorrect response for static2/.")
+    test_failed = True;
+else:
+    print("SUCCESS: Static handler replied with the correct response for static2/.")
+
+# Static File handler test: mapping static1
+# Spawn a shell process to curl to static file handler endpoint with path under static1/
+static1_file_request_command = "curl -i localhost:8080/static1/test.txt"
+static1_file_request_proc = subprocess.Popen(static1_file_request_command, stdout=subprocess.PIPE, shell=True)
+static1_response = static1_file_request_proc.stdout.read().decode('utf-8')
+
+expected_static1_response = """HTTP/1.1 200 OK\r
+Content-Type: text/plain\r
+\r
+HEY!\n\n"""
+
+if static1_response != expected_static1_response:
+    print("ERROR: Static handler replied with an incorrect response for static1/.")
+    test_failed = True;
+else:
+    print("SUCCESS: Static handler replied with the correct response for static1/.")
+
+# Static File handler test: 404 Not Found
+# Spawn a shell process to curl to static file handler with nonexistent file path
+static404_file_request_command = "curl -i localhost:8080/no_file"
+static404_file_request_proc = subprocess.Popen(static404_file_request_command, stdout=subprocess.PIPE, shell=True)
+static404_response = static404_file_request_proc.stdout.read().decode('utf-8')
+
+expected_static404_response = """HTTP/1.1 404 Not Found\r
+Content-Type: text/plain\r
+\r
+404 File Not Found\n"""
+
+if static404_response != expected_static404_response:
+    print("ERROR: Static handler replied with an incorrect response for a 404 request")
+    test_failed = True;
+else:
+    print("SUCCESS: Static handler replied with the correct response for a 404 request.")
+
 # Terminate server process before exiting
 os.killpg(os.getpgid(server_proc.pid), signal.SIGTERM)
 
-if response != expected_response:
-    print("Server replied with an incorrect response.")
-    exit(1)
+if test_failed:
+	print("At least one test case failed. Exiting with exit code 1")
+	exit(1)
 else:
-    print("Server replied with the correct response. Successful test.")
-    exit(0)
+	print("All test cases passed. Exiting with exit code 0")
+	exit(0)
