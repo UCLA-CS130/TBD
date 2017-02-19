cd8483b4b84bc7dd14e7f31ed2a16eb4dd5a8eb5
alex m waz <alexmwaz@g.ucla.edu>
2017-02-19 01:19:55 -0800

    Refactor: Added Request and Response classes to Request Handler TODO: * remove HttpRequest class * write new unit tests for Request and Response * refactor handlers to use Request and Response

--- request_handler.cc
+++ request_handler.cc
@@ -1,4 +1,116 @@
+#include "iostream"
+#include "sstream"
 #include "request_handler.h"
+
+// Request Implementations
+
+std::unique_ptr<Request> Request::Parse(const std::string & raw_request) {
+    std::unique_ptr<Request> new_request(new Request());
+    new_request.raw_request_ = raw_request;
+    
+    Headers headers = new Headers();
+    std::vector<std::string> lines = split_lines(raw_request);
+    for (auto i = 0; i < lines.size() - 1; i++) {
+        if (i == 0) {
+        // parse first line of the request
+            auto first_space = lines[i].find(" ");
+            new_request.method_ = lines[i].substr(0, first_space);
+            std::string temp = lines[i].substr(first_space + 1);
+            auto second_space = temp.find(" ");
+            new_request.uri_ = temp.substr(0, second_space);
+            new_request.version_ = temp.substr(second_space + 1);
+        } else {
+        // parse header fields of the request
+            auto first_space = lines[i].find(" ");
+            std::string header = lines[i].substr(0, first_space - 1);
+            std::string field = lines[i].substr(first_space + 1);
+            headers.push_back(std::make_pair(header, field));
+        }
+    }
+    new_request.headers_ = headers;
+    
+    return new_request;
+}
+
+std::vector<std::string> Request::split_lines(std::string request) {
+    std::stringstream ss(request);
+    std::string line;
+    std::vector<std::string> lines;
+
+    if (!request.empty()) {
+        // tokenize each line by newline
+        while(std::getline(ss,line,'\n')){
+            // remove carriage return from line
+            lines.push_back(line.substr(0, line.size()-1));
+        }
+    }
+    return lines;
+}
+
+const std::string Request::raw_request() {
+    return raw_request_;
+}
+
+const std::string Request::method() {
+    return method_;
+}
+
+const std::string Request::uri() {
+    return uri_;
+}
+
+const std::string Request::version() {
+    return version_;
+}
+
+const Headers Request::headers() {
+    return headers_;
+}
+
+const std::string body() {
+    return body_;
+}
+
+
+// Response Implementations
+
+std::string Response::to_string() {
+    std::string response = build_status_line(status_code_);
+    response += build_header("Content-Type", mime_type_);
+    response += "\r\n" + response_body_;
+    return response;
+}
+
+std::string Response::build_status_line() {
+    std::string status_line = "HTTP/1.1";
+
+    if (status_code_ == 200) {
+        status_line += " 200 OK";
+    } else if (status_code_ == 404) {
+        status_line += " 404 Not Found";
+    }
+
+    return status_line + "\r\n";
+}
+
+std::string Response::build_header(std::string field, std::string value) {
+    return field + ": " + value + "\r\n";
+}
+
+void Response::set_response_body(const std::string& response_body) {
+    response_body_ = response_body;
+}
+
+void Response::set_status_code(ResponseCode status_code) {
+    status_code_ = status_code;
+}
+
+void Response::set_mime_type(const std::string& mime_type) {
+    mime_type_ = mime_type;
+}
+
+
+// RequestHandler Implementations
 
 RequestHandler::RequestHandler() {}
 
--- request_handler.h
+++ request_handler.h
@@ -2,7 +2,73 @@
 #define REQUEST_HANDLER_H
 
 #include <string>
+#include <vector>
+#include <memory>
+#include <utility>
 
+// Represents an HTTP Request.
+// 
+// Usage:
+// auto request = Request::Parse(raw_request);
+class Request {
+public:
+    static unique_ptr<Request> Parse(const std::string& raw_request);
+
+    std::string raw_request() const;
+    std::string method() const;
+    std::string uri() const;
+    std::string version() const;
+
+    using Headers = std::vector<std::pair<std::string, std::string>>;
+    Headers headers() const;
+
+    std::string body() const;
+
+private:
+    std::vector<std::string> split_lines(std::string request);
+
+    std::string raw_request_;
+    std::string method_;
+    std::string uri_;
+    std::string version_;
+    Headers headers_;
+    std::string body_;
+};
+
+// Represents an HTTP response.
+// 
+// Usage:
+//     Response r;
+//     r.SetStatus(RESPONSE_200);
+//     r.SetBody(...);
+//     return r.ToString();
+// 
+// Constructed by the RequestHandler, after which the server should call ToString
+// to serialize.
+class Response {
+public:
+    enum ResponseCode {
+        RESPONSE_200 = 200;
+        RESPONSE_404 = 404;
+    };
+
+    std::string to_string();
+    void set_response_body(const std::string& response_body);
+    void set_status_code(ResponseCode status_code);
+
+    void set_mime_type(const std::string& mime_type);
+
+private:
+    std::string build_status_line();
+    std::string build_header(std::string field, std::string value);
+    
+    std::string response_body_;
+    std::string mime_type_ = "text/plain";
+    int status_code_ = 404; // default to error
+};
+
+// Represents the parent of all request handlers. Implementations should expect to
+// be long lived and created at server constrution.
 class RequestHandler {
 public:
     RequestHandler();
