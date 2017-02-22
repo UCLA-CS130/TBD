03c566dd107689566235cd1e16e15b9d5c092593
alex m waz <alexmwaz@g.ucla.edu>
2017-02-22 15:34:27 -0800

    Update EchoHandlerTest for more test cases

--- echo_handler_test.cc
+++ echo_handler_test.cc
@@ -6,9 +6,21 @@ protected:
     std::string GetName() {
         return echo_handler_.GetName();
     }
+    std::string HandleRequest(const std::string& raw_request) {
+        auto request = Request::Parse(raw_request);
+        Response response;
+        echo_handler_.HandleRequest(*request, &response);
+        return response.ToString();
+    }
     EchoHandler echo_handler_;
 };
 
 TEST_F(EchoHandlerTest, GetNameCheck) {
     EXPECT_EQ("EchoHandler", GetName());
 }
+
+TEST_F(EchoHandlerTest, HandleEchoRequest) {
+    EXPECT_EQ("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\n\r\nGET / HTTP/1.1",
+            HandleRequest("GET / HTTP/1.1"));
+}
+
