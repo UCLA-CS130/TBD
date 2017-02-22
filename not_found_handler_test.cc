06992790ea24fd45dea7c2e6e22ccf0623c48517
alex m waz <alexmwaz@g.ucla.edu>
2017-02-22 15:42:39 -0800

    Update NotFoundHandler for more test cases

--- not_found_handler_test.cc
+++ not_found_handler_test.cc
@@ -7,6 +7,12 @@ protected:
     std::string GetName() {
         return not_found_handler_.GetName();
     }
+    std::string HandleRequest() {
+        Request* req = nullptr;
+        Response response;
+        not_found_handler_.HandleRequest(*req, &response);
+        return response.ToString();
+    }
     NotFoundHandler not_found_handler_;
 };
 
@@ -14,3 +20,7 @@ TEST_F(NotFoundHandlerTest, GetNameCheck) {
     EXPECT_EQ("NotFoundHandler", GetName());
 }
 
+TEST_F(NotFoundHandlerTest, HandleNotFound) {
+    EXPECT_EQ("HTTP/1.1 404 Not Found\r\nContent-Type: text/plain\r\n\r\n404 File Not Found\n",
+            HandleRequest());
+}
