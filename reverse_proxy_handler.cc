b0b0e56925ffa0fe7b755b4920c8afb32d1baa2a
Jordan <jpmemorize@ucla.edu>
2017-02-25 17:00:05 -0800

    Made it so reverse proxy always routes to echo uri of remote server; so far, just returning the whole response instead of modifying it

--- reverse_proxy_handler.cc
+++ reverse_proxy_handler.cc
@@ -1,5 +1,11 @@
 #include "reverse_proxy_handler.h"
 
+#include <iostream>
+#include <string>
+#include <boost/array.hpp>
+#include <boost/asio.hpp>
+using boost::asio::ip::tcp;
+
 ReverseProxyHandler::ReverseProxyHandler() {}
 
 ReverseProxyHandler::~ReverseProxyHandler() {}
@@ -39,9 +45,75 @@ ReverseProxyHandler::Status ReverseProxyHandler::Init(const std::string& uri_pre
 
 ReverseProxyHandler::Status ReverseProxyHandler::HandleRequest(const Request& request, Response* response) {
     // TODO: Implement reverse proxy request
+
+
+
+    boost::asio::io_service io_service;
+
+    tcp::resolver resolver(io_service);
+    tcp::resolver::query query(remote_host_, std::to_string(remote_port_));
+    tcp::resolver::iterator endpoint_iterator = resolver.resolve(query);
+    tcp::resolver::iterator iter = endpoint_iterator;
+    tcp::resolver::iterator end; // End marker.
+    while (iter != end)
+    {
+      tcp::endpoint endpoint = *iter++;
+      std::cout << endpoint << std::endl;
+    }
+
+    tcp::socket socket(io_service);
+    boost::asio::connect(socket, endpoint_iterator);
+
+    std::cout << "Successfully connected!\n";
+
+    std::string request_string = std::string("GET /echo HTTP/1.1\r\n") +
+                          "Host: " + remote_host_ + "\r\n" +
+                          "Connection: keep-alive\r\n" +
+                          "Accept-Encoding: gzip, deflate, sdch, br\r\n" +
+                          "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8\r\n" +
+                          "\r\n";
+
+
+     // Send the response back to the client and then we're done
+    socket.write_some(boost::asio::buffer(request_string, request_string.size()));
+
+    std::string remote_response;
+
+    while(1)
+    {
+      
+        boost::array<char, 128> buf;
+        boost::system::error_code error;
+
+        size_t len = socket.read_some(boost::asio::buffer(buf), error);
+
+        //std::cout << "Got here!\n";
+        if (error == boost::asio::error::eof) {
+            std::cout <<"Exited cleanly\n";
+            break; // Connection closed cleanly by peer.
+        }
+        else if (error)
+            throw boost::system::system_error(error); // Some other error.
+
+
+        
+        //remote_response += std::string(std::begin(buf), std::end(buf));
+        std::cout.write(buf.data(), len);
+        remote_response += std::string(buf.data(), len);
+
+        
+    }
+
+    std::cout << remote_response << std::endl;
+    //std::unique_ptr<Request> response_as_request = Request::Parse(remote_response);
+
     response->SetStatus(Response::ResponseCode::OK);
+
+
+
     response->AddHeader("Content-Type", "text/plain");
     response->SetBody(request.raw_request());
+    response->SetBody(remote_response);
     return Status::OK;
 }
 
