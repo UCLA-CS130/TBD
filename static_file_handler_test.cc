31f4bb0ce3d261eee43cfffeb5be6c297ea0505a
Jason Yoon <jasony0206@gmail.com>
2017-03-11 18:43:48 +0000

    Add tests for StaticHandler::ProcessMarkdown()

--- static_file_handler_test.cc
+++ static_file_handler_test.cc
@@ -19,6 +19,9 @@ protected:
         static_file_handler_.HandleRequest(*request, &response);
         return response.ToString();
     }
+    std::string ProcessMarkdown(const std::string& content) {
+        return static_file_handler_.ProcessMarkdown(content);
+    }
     StaticFileHandler static_file_handler_;
 };
 
@@ -42,6 +45,9 @@ TEST_F(StaticFileHandlerTest, GetMimeTypeTestDefault) {
     EXPECT_EQ("text/plain", GetMimeType("turtle"));
 }
 
+TEST_F(StaticFileHandlerTest, GetMimeTypeTestMarkdown) {
+    EXPECT_EQ("text/html", GetMimeType("lama.md"));
+}
 
 TEST_F(StaticFileHandlerTest, ReadFileSuccessTest) {
     std::string file_content;
@@ -57,4 +63,10 @@ TEST_F(StaticFileHandlerTest, ReadFileErrorTest) {
 
 TEST_F(StaticFileHandlerTest, HandleFailFileRequest) {
     EXPECT_EQ("HTTP/1.1 404 Not Found\r\n\r\n", HandleRequest("GET /fail.txt HTTP/1.1"));
+}
+
+TEST_F(StaticFileHandlerTest, ProcessMarkdownTest) {
+    std::string content = "# Title\n## Heading\nBody";
+    std::string expected = "<h1>Title</h1>\n<h2>Heading</h2>\n<p>Body</p>\n\n";
+    EXPECT_EQ(expected, ProcessMarkdown(content));
 }
