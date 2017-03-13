2354c60a06fae40415fb17c1e3d91c1d6fc93d3a
alex m waz <alexmwaz@g.ucla.edu>
2017-03-12 17:04:43 -0700

    Updated EchoHandlerTest and ServerTest to reflect gzip encoding

--- echo_handler_test.cc
+++ echo_handler_test.cc
@@ -20,7 +20,7 @@ TEST_F(EchoHandlerTest, GetNameCheck) {
 }
 
 TEST_F(EchoHandlerTest, HandleEchoRequest) {
-    EXPECT_EQ("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\n\r\nGET / HTTP/1.1",
-            HandleRequest("GET / HTTP/1.1"));
+    EXPECT_STREQ("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Encoding: gzip\r\n\r\n\x1F\x8B\b\0\0\0\0\0\x2\xFFsw\rQ\xD0O\xCB\xCFW\xF0\b\t\t\xD0\" \"7\xD4\" \"3\xE4\xE5\xF2\xC8/.\xB1R\xC8\xC9ON\xCC\xC9\01-\f,\fx\xB9x\xB9\0;\xEF(\x9D+\0\0\0",
+            HandleRequest("GET / HTTP/1.1").c_str());
 }
 
